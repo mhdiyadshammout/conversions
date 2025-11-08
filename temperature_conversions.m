a16bcb807962c62f2cdc83b0379431204530b153
Mhd Iyad Shammout <mhdiyad.shammout@postgrad.manchester.ac.uk>
2025-11-08 22:27:00 +0000

    Add Newton to Celsius conversion

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -1,3 +1,17 @@
+%TEMPERATURE_CONVERSIONS
+% Check temperature conversions between Kelvin, Fahrenheit, Celsius and
+% Newton
+
+% Save degree symbol as a variable
+deg = char(176);
+
+disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
+fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
+fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
+fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg)
+
+fprintf('33 %sN is %g%sC.\n', deg, newton_to_celsius(33), deg)
+
 function fahrenheit = celsius_to_fahrenheit(celsius)
     %CELSIUS_TO_FAHRENHEIT   Convert Celsius to Fahrenheit 
 
