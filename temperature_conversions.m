1d34254cb78b5bfb2897fd53c4125fa991ca16d1
Mhd Iyad Shammout <mhdiyad.shammout@postgrad.manchester.ac.uk>
2025-11-08 21:55:57 +0000

    Include Kelvin conversion

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -3,5 +3,6 @@ function fahrenheit = celsius_to_fahrenheit(celsius)
 
     fahrenheit = celsius*(9/5) + 32;
 fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
+fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 
 end
