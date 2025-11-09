f46523b8ba9676a93c66bffb3a7dee81dabe88a9
Mhd Iyad Shammout <mhdiyad.shammout@postgrad.manchester.ac.uk>
2025-11-09 00:21:15 +0000

    Clarify script behaviour in H1 line

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -1,5 +1,7 @@
-function fahrenheit = celsius_to_fahrenheit(celsius)
-    %CELSIUS_TO_FAHRENHEIT   Convert Celsius to Fahrenheit 
+%TEMPERATURE_CONVERSIONS
+% Print key temperature conversions between Kelvin, Fahrenheit, Celsius and
+% Newton
+
 
     fahrenheit = celsius*(9/5) + 32;
 fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
